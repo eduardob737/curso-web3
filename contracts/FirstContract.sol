420f25503473825531b7661e17ad668014ba4646
Eduardo Barbosa <eduardobabrosalhp@gmail.com>
2023-03-17 15:56:56 -0300

    updating smart contract

--- contracts/FirstContract.sol
+++ contracts/FirstContract.sol
@@ -5,28 +5,25 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract FirstContract{
 
-    uint price = 10;
     address public owner;
             
-      constructor(){
+    constructor(){
       owner = msg.sender;
-    }   
-      modifier _ownerOnly(){
-      require(msg.sender == owner , "Sender is not owner!");
+    }
+    
+    modifier _onlyOwner(){
+      require(msg.sender == owner, "Sender is not owner!");
       _;
-}
+    }
 
     function getContract() public view returns(address) {
             return owner;
-        }
-
-    function functionOnlyOwnerCanCall() public _ownerOnly {
-        
     }
 
-    function changePrice (uint newPrice) public _ownerOnly returns (string memory message) {
-        price = newPrice;
-        message = string.concat("Novo preco alterado: ", Strings.toString(price));
-        return message;
+    function getSender() public view returns(address) {
+            return msg.sender;
+    }
+
+    function functionOnlyOwnerCanCall() public _onlyOwner {    
     }
 }
--- test/first_contract.teste.js
+++ test/first_contract.teste.js
@@ -3,15 +3,19 @@ const FirstContract = artifacts.require("FirstContract");
 contract ('FirstContract', function (accounts){
     let contractInstance;
     let owner;
+    let sender;
 
     before(async () => {
         contractInstance = await FirstContract.deployed();
         owner = await contractInstance.getContract();
-      });
+        sender = await contractInstance.getSender();
 
+        console.log("Owner: " + owner);
+        console.log("Sender: " + sender);
+      });
+      
     it("should deploy the contract and allow the user", async () => {
         const result = await contractInstance.functionOnlyOwnerCanCall({ from: owner });
     assert.equal(result, result, 'Function did not execute as expected');
   });
-
 })
